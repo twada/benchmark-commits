9eedf7f67733b4c18a4bcd5768007972ba71df1e
Takuto Wada <takuto.wada@gmail.com>
2021-10-11 11:18:08 +0900

    refactor: delay benchmark registration to simplify async error handling

--- suite-setup.js
+++ suite-setup.js
@@ -6,10 +6,6 @@ const { spawn } = require('child_process');
 const { extract } = require('extract-git-treeish');
 const zf = (n, len = 2) => String(n).padStart(len, '0');
 const ymd = (d = new Date()) => `${d.getFullYear()}${zf(d.getMonth() + 1)}${zf(d.getDate())}${zf(d.getHours())}${zf(d.getMinutes())}${zf(d.getSeconds())}${zf(d.getMilliseconds(), 3)}`;
-const isPromiseLike = (o) => o !== null &&
-      typeof o === 'object' &&
-      typeof o.then === 'function' &&
-      typeof o.catch === 'function';
 
 class SuiteSetup extends EventEmitter {
   constructor(suite, workDir) {
@@ -23,7 +19,7 @@ class SuiteSetup extends EventEmitter {
     const suite = this.suite;
     setup.emit('start', specs);
 
-    const installations = specs.map((spec) => {
+    const preparations = specs.map((spec) => {
       return new Promise((resolve, reject) => {
         extract({ treeIsh: spec.git, dest: join(destDir, spec.name) }).then(({ dir }) => {
           setup.emit('npm:install:start', spec, dir);
@@ -38,32 +34,20 @@ class SuiteSetup extends EventEmitter {
               });
         }).catch(reject);
       });
-    });
-
-    const registrations = installations.map((installation) => {
+    }).map((installation) => {
       return installation.then(({spec, dir}) => {
-        const addToSuite = (fn) => {
-          if (typeof fn === 'function') {
-            suite.add(specDesc(spec), fn);
-          }
-          return {spec, dir};
-        };
         setup.emit('register', spec, dir);
-        // rejects benchmark registration Promise on error
-        const fn = register({ suite, spec, dir });
-        if (isPromiseLike(fn)) {
-          // rejects benchmark registration Promise if fn rejects
-          return fn.then((resolved) => addToSuite(resolved));
-        } else {
-          return addToSuite(fn);
-        }
+        return register({ suite, spec, dir });
       });
     });
 
-    return Promise.allSettled(registrations).then(results => {
+    return Promise.allSettled(preparations).then(results => {
       specs.forEach((spec, i) => {
         const result = results[i];
-        if (result.status === 'rejected') {
+        if (result.status === 'fulfilled') {
+          const fn = result.value;
+          suite.add(specDesc(spec), fn);
+        } else if (result.status === 'rejected') {
           setup.emit('skip', spec, result.reason);
         }
       });
